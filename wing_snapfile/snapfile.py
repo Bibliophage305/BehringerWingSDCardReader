78e97e91d58d76fad274776fe277aa8ae4368bc5
Chris <christopher.hampson58@gmail.com>
2026-05-26 21:59:09 +0100

    Make a key name more explicit

--- wing_snapfile/snapfile.py
+++ wing_snapfile/snapfile.py
@@ -8,7 +8,7 @@ from wing_snapfile.audio.data import AudioData
 class Snapfile:
     type: str
     creator: str
-    creator_vers: str
+    creator_version: str
     creator_model: str
     creator_name: str
     audio_engine_data: AudioData
@@ -19,7 +19,7 @@ class Snapfile:
         return Snapfile(
             type=data["type"],
             creator=data["creator"],
-            creator_vers=data["creator_vers"],
+            creator_version=data["creator_vers"],
             creator_model=data["creator_model"],
             creator_name=data["creator_name"],
             audio_engine_data=AudioData.from_dict(data["ae_data"]),
@@ -30,7 +30,7 @@ class Snapfile:
         return {
             "type": self.type,
             "creator": self.creator,
-            "creator_vers": self.creator_vers,
+            "creator_vers": self.creator_version,
             "creator_model": self.creator_model,
             "creator_name": self.creator_name,
             "ae_data": self.audio_engine_data.to_dict(),
